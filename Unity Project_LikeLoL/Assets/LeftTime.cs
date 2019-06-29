d731e21b015cdcc8350c747eebc0ae56563fcb0e
Haeinna <epdlwl3929@naver.com>
2019-06-29 10:06:16 +0900

    0629_UI

---
+++ Unity Project_LikeLoL/Assets/LeftTime.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class LeftTime : MonoBehaviour {
+    
+    public Text Time;
+    int oneSecond = 5;
+    IEnumerator coroutine;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine("Timer");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Time.text = ""+oneSecond;
+	}
+
+    IEnumerator Timer() {
+        if (oneSecond == 0)
+            StopCoroutine(coroutine);
+        yield return new WaitForSeconds(1f);
+        oneSecond--;
+        StartCoroutine("Timer");
+    }
+}
