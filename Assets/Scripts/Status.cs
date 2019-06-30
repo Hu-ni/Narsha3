30549963f0a6c00582c37aa7268aff36fbf0b021
Hu-ni <dev-game@naver.com>
2019-06-30 09:25:56 +0900

    Scripts PlayerController 이동 공격 수정

---
+++ Assets/Scripts/Status.cs
@@ -0,0 +1,14 @@
+using System;
+
+[Serializable]
+public struct Status
+{
+    public float hp;
+    public float mp;
+    public float power;
+    public float attackSpeed;
+    public float attackRange;
+    public float speed;
+    public int level;
+    public int exp;
+}
